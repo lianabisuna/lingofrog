f07c3e14b6c63581544528deccbb12dbbd33d3dc
Liana Bisuña <lianabisuna@gmail.com>
2021-09-29 23:06:54 +0800

    Change card hover effect -button card at column end

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,65 +1,70 @@
 <template>
   <v-hover v-slot="{ hover }">
-    <v-card
+    <v-sheet
+      outlined
+      :color="hover ? 'orange' : 'lightgrey'"
       class="rounded-lg flex-grow-1 d-flex flex-column"
-      :flat="!hover"
-      :style="{ border: '1px solid lightgrey !important' }"
     >
-      <div>
-        <v-toolbar
-          dense
-          flat
-          class="rounded-lg"
-          color="transparent"
-        >
-          <v-row
-            align="center"
-            no-gutters
-          >
-            <v-avatar
-              class="mr-2"
-              size="20"
-            >
-              <img
-                :src="src"
-                alt="John"
-              >
-            </v-avatar>
-            <span>{{ language }}</span>
-
-            <span v-if="hover" style="position: absolute; right: -0.75rem; top: -0.75rem;">
-              <v-btn
-                fab
-                x-small
-                depressed
-                color="white"
-                :style="{ border: '1px solid lightgrey !important' }"
-              >
-                <v-icon>mdi-close</v-icon>
-              </v-btn>
-            </span>
-          </v-row>
-        </v-toolbar>
-      </div>
-
-      <v-card-text
-        class="text-h2 black--text"
-        :style="{ minHeight: '20vh' }"
+      <v-card
+        class="rounded-lg flex-grow-1 d-flex flex-column"
+        flat
       >
-        {{ translation }}
-      </v-card-text>
+        <div>
+          <v-toolbar
+            dense
+            flat
+            class="rounded-lg"
+            color="transparent"
+          >
+            <v-row
+              align="center"
+              no-gutters
+            >
+              <v-avatar
+                class="mr-2"
+                size="20"
+              >
+                <img
+                  :src="src"
+                  alt="John"
+                >
+              </v-avatar>
+              <span>{{ language }}</span>
 
-      <v-spacer></v-spacer>
-      
-      <v-card-actions>
-        <v-btn
-          text
-          icon
+              <span v-if="hover" style="position: absolute; right: -0.75rem; top: -0.75rem;">
+                <v-btn
+                  fab
+                  x-small
+                  depressed
+                  color="white"
+                  :style="{ border: '1px solid orange !important' }"
+                >
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </span>
+            </v-row>
+          </v-toolbar>
+        </div>
+
+        <v-card-text
+          class="text-h2 black--text"
+          :style="{ minHeight: '25vh' }"
         >
-          <v-icon>mdi-volume-high</v-icon>
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+          {{ translation }}
+        </v-card-text>
+
+        <v-spacer></v-spacer>
+        
+        <v-card-actions>
+          <v-btn
+            text
+            icon
+          >
+            <v-icon>mdi-volume-high</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-sheet>
   </v-hover>
 </template>
 
---
+++ src/components/ButtonCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-sheet
+      outlined
+      :color="lightgrey"
+      class="rounded-lg flex-grow-1 d-flex flex-column"
+    >
+      <v-card
+        class="rounded-lg flex-grow-1 align-center d-flex"
+        hover
+        flat
+        :color="hover ? 'grey lighten-5' : ''"
+        @click="openLanguageBar"
+        :ripple="hover"
+      >
+        <v-card-text class="text-center">
+          <v-icon x-large>mdi-plus</v-icon>
+        </v-card-text>
+      </v-card>
+    </v-sheet>
+  </v-hover>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonCard'
+  }
+</script>
